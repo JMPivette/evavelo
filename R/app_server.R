9b5113b29ea78ca0a7ee828671c4b875cd257744
Jean-Marie Pivette <jpivette@gmail.com>
2021-02-25 12:18:44 +0100

    Fix issue of logos not appearing at startup

--- R/app_server.R
+++ R/app_server.R
@@ -7,8 +7,6 @@
 app_server <- function( input, output, session ) {
   options(shiny.maxRequestSize=30*1024^2) ## Allow file upload up to 30MB To be adjusted if needed
 
-  addResourcePath('img', system.file('app/img', package = 'evavelo'))
-
   ## Small r strategy
   r <- reactiveValues()
   # List the first level callModules here
