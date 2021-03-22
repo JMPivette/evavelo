e57c3bc885bf078f875bb402a6e9ed472c2a4b63
Jean-Marie Pivette <jpivette@gmail.com>
2021-03-22 17:42:48 +0100

    Improve check_similar_enquete() following last meeting

--- R/check_similar_enquete.R
+++ R/check_similar_enquete.R
@@ -10,17 +10,25 @@
 #' @export
 
 check_similar_enquete <- function(enquete){
-  char_variables_to_check <- c("id_site_enq", "date_enq","ville_res", "mode_heb", "ville_heb",
+  ## Mandatory Identical answers within a group.
+  mand_variables_to_check <- c("id_site_enq", "date_enq", "mode_heb", "ville_heb",
                                "type_groupe", "type_trajet", "type_sortie",
                                "iti_depart_initial", "iti_depart_itineraire", "iti_arrivee_itineraire", "iti_arrivee_final")
+  ## Not necessary identical within a group
+  opt_variables_to_check <- c("heure_enq", "age", "km_sortie", "ville_res")
 
-  num_variables_to_check <- c("heure_enq", "age", "km_sortie")
 
   ## Inner algorithm function to define similar answers from numeric answers
-  keep_potential_duplicated <- function(nb_na, heure_enq, age, km_sortie){
+  keep_potential_duplicated <- function(nb_na, heure_enq, age, km_sortie, ville_res){
     # Case where a lot of answers are not missing
-    if(unique(nb_na) <=5) return(TRUE)
+    if(unique(nb_na) <=4) return(TRUE)
 
+    # Allow an additional missing answer if ville_res is the same
+    ville_res <- unique(ville_res)
+    if(unique(nb_na) == 5 && !is.na(ville_res) && length(ville_res) == 1)
+      return(TRUE)
+
+    # For the other cases we use the km_sortie and heure_diff information.
     age_diff <- max(age) - min(age)
     km_diff <- (max(km_sortie) - min(km_sortie)) / max(km_sortie)
     heure_diff <- max(heure_enq) - min(heure_enq)
@@ -37,27 +45,26 @@ check_similar_enquete <- function(enquete){
  log <- "Recherche de questionnaires multiples...\n------------------------------------------\n"
 
   simil_enq <- enquete %>%
-    dplyr::filter(.data$id_quest == radical_quest(.data$id_quest)) %>% ## Remove multiple quest from analysis
     dplyr::select(
       .data$id_quest, .data$date_enq,
-      dplyr::all_of(char_variables_to_check),
-      dplyr::all_of(num_variables_to_check)
+      dplyr::all_of(mand_variables_to_check),
+      dplyr::all_of(opt_variables_to_check)
     ) %>%
     dplyr::filter(.data$type_groupe != "Seul") %>% ## remove group defined as alone
     dplyr::mutate(## Count number of non-answers
-      nb_na = rowSums(is.na(dplyr::across(dplyr::all_of(char_variables_to_check))))
+      nb_na = rowSums(is.na(dplyr::across(dplyr::all_of(mand_variables_to_check))))
     ) %>%
     dplyr::group_by(
-      dplyr::across(dplyr::all_of(char_variables_to_check))
+      dplyr::across(dplyr::all_of(mand_variables_to_check))
     ) %>%
     filter(dplyr::n()>1) %>% ## keep only identical answers in multiple lines
-    dplyr::mutate(
-      res = keep_potential_duplicated(.data$nb_na,.data$heure_enq, .data$age, .data$km_sortie)
-    ) %>%
     dplyr::summarise(enq_list = paste0(.data$id_quest, collapse = ", "),
-                     res = sum(.data$res)) %>%
+                     res = keep_potential_duplicated(.data$nb_na,.data$heure_enq,
+                                                     .data$age, .data$km_sortie, .data$ville_res),
+                     ## Spot groups that were already identified
+                     n_radical = length(unique(radical_quest(.data$id_quest)))) %>%
     dplyr::ungroup() %>%
-    filter(.data$res >= 1) %>%
+    filter(.data$res == TRUE & .data$n_radical > 1) %>%
     select(.data$enq_list)
 
   if(nrow(simil_enq) !=0){
